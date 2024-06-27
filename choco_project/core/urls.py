9cb97373773eb42afa33af13b379b66412ccc89c
alvaro0219 <alvaroconti05@gmail.com>
2024-06-27 10:50:58 -0300

    Creando template base

--- choco_project/core/urls.py
+++ choco_project/core/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.hola_mundo, name='hola_mundo'),
+    path('', views.home, name='home'),
 ]
--- choco_project/core/views.py
+++ choco_project/core/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
-def hola_mundo(request):
-    return HttpResponse("Hola Mundo")
+def home(request):
+    return render(request, 'core/base.html')
