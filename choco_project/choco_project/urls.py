9299f44e7ab375cbec993ef498df86bf6d3b0f62
alvaro0219 <alvaroconti05@gmail.com>
2024-08-08 11:41:34 -0300

    Dando estilo a archivo base y home

--- choco_project/choco_project/urls.py
+++ choco_project/choco_project/urls.py
@@ -1,7 +1,12 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),  # URL principal de la aplicación
+    path('', include('core.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- choco_project/core/views.py
+++ choco_project/core/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'core/base.html')
+    return render(request, 'core/home.html')
