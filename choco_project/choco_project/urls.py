7ce983f57778c9e91ad65660a6f969ad1d2dbd48
FacuBaldo <facubaldo9@gmail.com>
2024-10-20 20:45:46 -0300

    Agregando login como vista principal

--- choco_project/choco_project/urls.py
+++ choco_project/choco_project/urls.py
@@ -2,9 +2,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', auth_views.LoginView.as_view(), name='login'),  # Redirige a login al inicio
     path('', include('core.urls')), 
     path('productos/', include('productos.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
